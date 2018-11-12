2c2b5a2e80acb0f7c7107ad927e880f95e4b6d0c
sonnyshih <sonnyshih@gmail.com>
2018-11-12 11:31:30 +0800

    Modify the Delegation pattern (委託模式).

--- src/main/java/com/DesignPattern/DelegationPattern/MyExercise/EmployeeDemo.java
+++ src/main/java/com/DesignPattern/DelegationPattern/MyExercise/EmployeeDemo.java
@@ -1,5 +1,6 @@
 package com.DesignPattern.DelegationPattern.MyExercise;
 
+import static com.DesignPattern.DelegationPattern.MyExercise.EmployeeLookUp.EmployeeType.PASSENGER;
 import static com.DesignPattern.DelegationPattern.MyExercise.EmployeeLookUp.EmployeeType.TICKET_AGENT;
 
 public class EmployeeDemo {
@@ -8,10 +9,19 @@ public class EmployeeDemo {
 
         showTicketAgentInfo();
 
+        System.out.println(" ###### ");
+
+        showPassengerInfo();
     }
 
     public static void showTicketAgentInfo(){
         EmployeeDelegate employeeDelegate = new EmployeeDelegate(TICKET_AGENT);
+        Employee employee = new Employee(employeeDelegate);
+        employee.showEmployeeInfo();
+    }
+
+    public static void showPassengerInfo(){
+        EmployeeDelegate employeeDelegate = new EmployeeDelegate(PASSENGER);
         Employee employee = new Employee(employeeDelegate);
         employee.showEmployeeInfo();
     }
